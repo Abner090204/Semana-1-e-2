f0e17266488371600979252d9869fb619df57a53
Abner090204 <160682626+Abner090204@users.noreply.github.com>
2025-12-11 16:51:42 -0300

    Create Produto.java

---
+++ Atividade1/Produto.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void exibirDados() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$ " + preco);
+        System.out.println("Quantidade: " + quantidade);
+        System.out.println("--------------------------");
+    }
+
+    public double valorTotalEmEstoque() {
+        return preco * quantidade;
+    }
+}
